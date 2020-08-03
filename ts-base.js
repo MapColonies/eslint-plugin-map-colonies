8b4cb529c4e2047909d9a983ff6a0e45ed23b666
CptSchnitz <12687466+CptSchnitz@users.noreply.github.com>
2020-08-03 16:31:43 +0300

    fix(ts-base): fixed PR

--- ts-base.js
+++ ts-base.js
@@ -1,54 +1,54 @@
 const namingConventions = [
-  "error",
+  'error',
   {
-    selector: "default",
-    format: ["camelCase"],
+    selector: 'default',
+    format: ['camelCase'],
   },
 
   {
-    selector: "variable",
-    format: ["camelCase", "UPPER_CASE"],
+    selector: 'variable',
+    format: ['camelCase', 'UPPER_CASE'],
   },
   {
-    selector: "variable",
-    format: ["PascalCase"],
+    selector: 'variable',
+    format: ['PascalCase'],
     custom: {
-      regex: "^.*Provider$",
+      regex: '^.*Provider$',
       match: false,
     },
   },
   {
-    selector: "parameter",
-    format: ["camelCase"],
-    leadingUnderscore: "allow",
+    selector: 'parameter',
+    format: ['camelCase'],
+    leadingUnderscore: 'allow',
   },
 
   {
-    selector: "memberLike",
-    modifiers: ["private"],
-    format: ["camelCase"],
+    selector: 'memberLike',
+    modifiers: ['private'],
+    format: ['camelCase'],
     // leadingUnderscore: 'require',
   },
 
   {
-    selector: "typeLike",
-    format: ["PascalCase"],
+    selector: 'typeLike',
+    format: ['PascalCase'],
   },
 ];
 
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier",
-    "prettier/@typescript-eslint",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
-  plugins: ["import", "@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
+  plugins: ['import', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
   env: {
     browser: true,
@@ -58,93 +58,98 @@ module.exports = {
   // overrides for TSX (mostly for react)
   overrides: [
     {
-      files: ["**/*.tsx"],
+      files: ['**/*.tsx'],
       rules: {
-        "@typescript-eslint/naming-convention": [
+        '@typescript-eslint/naming-convention': [
           ...namingConventions,
           {
-            selector: "variable",
-            types: ["function"],
-            format: ["camelCase", "PascalCase"],
+            selector: 'variable',
+            types: ['function'],
+            format: ['camelCase', 'PascalCase'],
           },
         ],
       },
     },
     // overrides for unit tests
     {
-      files: ["**/*.spec.ts?(x)", "**/*.spec.ts?(x)"],
+      files: ['**/*.spec.ts?(x)', '**/*.test.ts?(x)'],
       rules: {
-        "@typescript-eslint/no-magic-numbers": "off",
-        "@typescript-eslint/explicit-function-return-type": "off"
+        '@typescript-eslint/no-magic-numbers': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
       },
     },
   ],
   rules: {
-    "@typescript-eslint/array-type": ["error", { default: "array" }],
+    '@typescript-eslint/array-type': ['error', { default: 'array' }],
 
-    "@typescript-eslint/ban-ts-comment": "warn",
+    '@typescript-eslint/ban-ts-comment': 'warn',
 
-    "@typescript-eslint/ban-tslint-comment": "error",
+    '@typescript-eslint/ban-tslint-comment': 'error',
 
-    "@typescript-eslint/ban-types": "error",
+    '@typescript-eslint/ban-types': 'error',
 
-    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
 
-    "@typescript-eslint/explicit-function-return-type": "warn",
+    '@typescript-eslint/explicit-function-return-type': 'warn',
 
-    "@typescript-eslint/explicit-member-accessibility": "error",
+    '@typescript-eslint/explicit-member-accessibility': 'error',
 
-    "@typescript-eslint/member-ordering": "warn",
+    '@typescript-eslint/member-ordering': 'warn',
 
-    "@typescript-eslint/method-signature-style": "error",
+    '@typescript-eslint/method-signature-style': 'error',
 
-    "@typescript-eslint/no-empty-interface": "off",
+    '@typescript-eslint/no-empty-interface': 'off',
 
-    camelcase: "off",
-    "@typescript-eslint/naming-convention": namingConventions,
+    camelcase: 'off',
+    '@typescript-eslint/naming-convention': namingConventions,
 
-    "@typescript-eslint/no-base-to-string": "warn",
+    '@typescript-eslint/no-base-to-string': 'warn',
 
-    "@typescript-eslint/no-confusing-non-null-assertion": "error",
+    '@typescript-eslint/no-confusing-non-null-assertion': 'error',
 
-    "@typescript-eslint/no-extraneous-class": "warn",
+    '@typescript-eslint/no-extraneous-class': 'warn',
 
-    "@typescript-eslint/no-throw-literal": "error",
+    '@typescript-eslint/no-throw-literal': 'error',
 
-    // '@typescript-eslint/no-type-alias': check more
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
 
-    "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+    '@typescript-eslint/no-unnecessary-condition': 'error',
 
-    "@typescript-eslint/no-unnecessary-condition": "error",
+    '@typescript-eslint/no-unnecessary-type-arguments': 'warn',
 
-    "@typescript-eslint/no-unnecessary-type-arguments": "warn",
+    '@typescript-eslint/prefer-literal-enum-member': 'error',
 
-    "@typescript-eslint/prefer-literal-enum-member": "error",
+    '@typescript-eslint/prefer-nullish-coalescing': 'warn',
 
-    "@typescript-eslint/prefer-nullish-coalescing": "warn",
+    '@typescript-eslint/prefer-optional-chain': 'warn',
 
-    "@typescript-eslint/prefer-optional-chain": "warn",
+    '@typescript-eslint/prefer-readonly': 'error',
 
-    "@typescript-eslint/prefer-readonly": "error",
+    '@typescript-eslint/prefer-string-starts-ends-with': 'error',
 
-    "@typescript-eslint/prefer-string-starts-ends-with": "error",
+    '@typescript-eslint/promise-function-async': 'error',
 
-    "@typescript-eslint/promise-function-async": "error",
+    '@typescript-eslint/require-array-sort-compare': 'warn',
 
-    "@typescript-eslint/require-array-sort-compare": "warn",
+    '@typescript-eslint/strict-boolean-expressions': 'warn',
 
-    "@typescript-eslint/strict-boolean-expressions": "warn",
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
 
-    "@typescript-eslint/switch-exhaustiveness-check": "error",
+    '@typescript-eslint/no-magic-numbers': [
+      error,
+      {
+        ignoreArrayIndexes: true,
+        ignore: [1],
+        ignoreNumericLiteralTypes: true,
+      },
+    ],
 
-    "@typescript-eslint/no-magic-numbers": [error, {ignoreArrayIndexes:true, ignore:[1], ignoreNumericLiteralTypes: true}],
+    '@typescript-eslint/default-param-last': 'error',
 
-    "@typescript-eslint/default-param-last": "error",
+    '@typescript-eslint/no-dupe-class-members': 'error',
 
-    "@typescript-eslint/no-dupe-class-members": "error",
+    '@typescript-eslint/no-unused-expressions': 'error',
 
-    "@typescript-eslint/no-unused-expressions": "error",
-
-    "@typescript-eslint/return-await": "error",
+    '@typescript-eslint/return-await': 'error',
   },
 };
